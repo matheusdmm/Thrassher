a0b528cb991560ab5bafc01704640a5c445b6d16
Matheus <mfonse0@gmail.com>
2021-01-18 23:56:58 -0300

    wip

---
+++ cmake-build-debug/CMakeFiles/Thrassher.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Thrassher.dir/main.c.obj"
+  "Thrassher.exe"
+  "Thrassher.exe.manifest"
+  "Thrassher.pdb"
+  "libThrassher.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Thrassher.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
